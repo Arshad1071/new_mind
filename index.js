fbebe0d12b0daed1649e3d02dfa3f87c8ef65f70
Arshad vp <Arshad1071@yahoo.in>
2025-02-12 20:46:10 +0530

    second build genarated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(5000, () => {
-  console.log(`Server is listening on port ${5000}`);
+app.listen(6000, () => {
+  console.log(`Server is listening on port 6000`);
 });
 
 
