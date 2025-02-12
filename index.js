35e1a1eb2b5324fa2c52a639bfbfc776ca9b634b
Arshad vp <Arshad1071@yahoo.in>
2025-02-12 20:38:31 +0530

    open port added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.listen(5000, () => {
+  console.log(`Server is listening on port ${5000}`);
+});
+
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
